3b1d04215bbc89e820f9b882c4fbae42884df36a
Damilola <damiyoung30@gmail.com>
2022-12-23 17:54:30 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,51 @@
+import { Footer, Navbar } from "../components";
+import styles from "../styles";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
+
+const Page = () => (
+  <div className="bg-purple-700 overflow-hidden ">
+    <div>
+      <Navbar />
+      <Hero />
+
+      <div className={`${styles.videoDiv}`}>
+        <video autoPlay loop muted className={`${styles.video}`}>
+          <source src="/bg-video-1.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+    <div className="relative">
+      <About />
+      <div className={`${styles.videoDiv}`}>
+        <video autoPlay loop muted className={`${styles.video}`}>
+          <source src="/bg-video-2.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div>
+    <World />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-05 z-0" />
+      <Feedback />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Page;
---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Ttebify',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Abuja Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Vadd Planet',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Isaac Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk richer',
+    subtitle:
+        '....',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the INSANENESS of the Metaverse',
+    subtitle:
+        '......',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        '........',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
